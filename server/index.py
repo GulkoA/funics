6f079105fb3d2d1a232c95f3ff6733b45a9c8301
Ramora0 <99ilovecats99@gmail.com>
2024-10-20 01:09:15 -0400

    Added functionality to record and send audio

--- server/index.py
+++ server/index.py
@@ -5,39 +5,43 @@ app = Flask(__name__)
 
 dist_folder = '../website/dist'
 
+
 @app.route("/")
 @app.route("/parent")
 def serve_html():
-  return send_from_directory(dist_folder, 'index.html')
+    return send_from_directory(dist_folder, 'index.html')
+
 
 @app.route("/assets/<path>")
 def serve_assets(path):
-  print('path requested', path)
-  return send_from_directory(dist_folder + '/assets', path)
+    print('path requested', path)
+    return send_from_directory(dist_folder + '/assets', path)
+
 
 @app.route("/cache/<path>")
 def cache(path):
-  return send_from_directory('../cache', path)
+    return send_from_directory('../cache', path)
+
 
 @app.route("/api/get-word")
 def get_word():
-  # get word from database
+    # get word from database
 
-  word = "hello"
-  audio_path = "cache/hello.mp3"
-  return {
-    "word": word,
-    "audioPath": audio_path
-  }
+    word = "hello"
+    return {
+        "word": word,
+        "audioURL": "https://media.merriam-webster.com/audio/prons/en/us/mp3/o/orange02.mp3"
+    }
 
 # post request
+
+
 @app.route("/api/submit-audio", methods=["POST"], )
 def submit_audio():
-  audio_file = request.files['audio']
-  # analyze audio
+    audio_file = request.files['audio']
+    # analyze audio
 
-
-  good = True
-  return {
-    "good": good
-  }
+    good = True
+    return {
+        "good": good
+    }
--- website/src/pages/Practice.tsx
+++ website/src/pages/Practice.tsx
@@ -3,30 +3,66 @@ import MicIcon from '@mui/icons-material/Mic';
 import StopCircleOutlinedIcon from '@mui/icons-material/StopCircleOutlined';
 import { useEffect, useState } from "react";
 import API from "../helpers/API";
+import { AudioRecorder, useAudioRecorder } from "react-audio-voice-recorder";
 
 export default function Practice({ setLoggedIn }: { setLoggedIn: (value: boolean) => void }) {
   const [listening, setListening] = useState(false);
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState("bob");
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+
+  const recorderControls = useAudioRecorder();
 
   useEffect(() => {
     (async () => {
-      setWord((await API.get_word()).word);
+      const wordData = await API.getWord();
+      setWord(wordData.word);
+      setAudio(new Audio(wordData.audioURL));
     })();
   }, []);
 
   function switchListening() {
+    if (listening) {
+      recorderControls.stopRecording();
+      console.log("Recording stopped");
+    } else {
+      recorderControls.startRecording();
+      console.log("Recording started");
+    }
     setListening((oldListening) => !oldListening);
+  }
+
+  function sendAudio(blob: Blob) {
+    // console.log("Sending audio");
+
+    // const audioUrl = URL.createObjectURL(blob);
+    // const audio = new Audio(audioUrl);
+
+    // console.log("Playing audio");
+
+    // audio.play();
+    // console.log("Audio played");
+
+    API.sendAudio(blob);
+  }
+
+  function sayWord() {
+    audio!.play();
   }
 
   return (
     <div className="page">
       <Header setLoggedIn={setLoggedIn} />
+
+      <div className="fill" />
+
       <p className="say">Say</p>
-      <div className="wordToSayBox">
-        <div className="wordToSayBox2">
-          <h1 className="wordToSay">{word}</h1>
+      {word === "" ? <p>Loading...</p> : (
+        <div className="wordToSayBox">
+          <div className="wordToSayBox2" onClick={() => sayWord()}>
+            <h1 className="wordToSay">{word}</h1>
+          </div>
         </div>
-      </div>
+      )}
 
       <div className="fill" />
 
@@ -37,6 +73,16 @@ export default function Practice({ setLoggedIn }: { setLoggedIn: (value: boolean
           (<StopCircleOutlinedIcon className="mic" style={{ fontSize: "100" }} />) :
           (<MicIcon className="stopMi" style={{ fontSize: "100" }} />)
         }
+      </div>
+      <div className="hidden">
+        <AudioRecorder
+          onRecordingComplete={sendAudio}
+          audioTrackConstraints={{
+            noiseSuppression: true,
+            echoCancellation: true,
+          }}
+          recorderControls={recorderControls}
+        />
       </div>
     </div>
   )
